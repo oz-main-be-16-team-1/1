3d3d1400d0c5aa6a403aa21e087e60feddd5429e
sangbo <tkdqh57@naver.com>
2026-02-19 15:49:59 +0900

    구조 수정 및 브랜치수정

--- Django_project/config/settings.py
+++ Django_project/config/settings.py
@@ -104,9 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ko-KR'
+TIME_ZONE = 'asia/Seoul'
 
 USE_I18N = True
 
