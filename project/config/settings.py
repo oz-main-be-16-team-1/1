3f7e62ba62ba9b371abd6d36c78492fead8fa3a2
wooryun <rhrjs4@naver.com>
2026-02-24 11:17:27 +0900

    ♻️ refactor : 어드민 페이지필터기능 추가

--- project/config/settings.py
+++ project/config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,12 +79,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', '기본DB이름'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'), # 기본값을 postgres로 지정
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '비밀번호'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
