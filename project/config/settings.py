bb906e9ccdf155c756d80fcd508436e398a351d7
wooryun <rhrjs4@naver.com>
2026-02-24 11:15:19 +0900

    ♻️ refactor : 어드민 페이지 기능 추가

--- project/config/settings.py
+++ project/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', '기본DB이름'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'), # 기본값을 postgres로 지정
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '비밀번호'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
