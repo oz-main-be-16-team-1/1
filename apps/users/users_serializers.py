d18209ac0f072dcf1fcc5caf7c13d105a8e9ed12
sangbo <tkdqh57@naver.com>
2026-02-27 10:33:15 +0900

    register에서 비밀번호 생성시 ***로 나오게 수정

--- apps/users/users_serializers.py
+++ apps/users/users_serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(
+        write_only=True,
+        style={'input_type': 'password'}
+    )
 
     class Meta:
         model = User
